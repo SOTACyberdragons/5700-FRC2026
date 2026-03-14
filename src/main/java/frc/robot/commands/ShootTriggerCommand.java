3dd1bd993eb5831057174eb03afd14755fbf1795
august-white <142743311+august-white@users.noreply.github.com>
2026-03-14 09:03:17 -0700

    update shooter command names

---
+++ src/main/java/frc/robot/commands/ShootTriggerCommand.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.ShooterSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ShootTriggerCommand extends Command {
+	private ShooterSubsystem m_shooterSubsystem;
+	private double percent;
+
+	private boolean isKilled = false;
+
+	/** Creates a new ShootCommand. */
+	public ShootTriggerCommand(ShooterSubsystem shooter, double percent) {
+		m_shooterSubsystem = shooter;
+		addRequirements(m_shooterSubsystem);
+		this.percent = percent;
+	}
+
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		m_shooterSubsystem.kickerMotorPercentOutput.withOutput(Constants.ShooterConstants.KICKER_PERCENT);
+		m_shooterSubsystem.kickerMotor.setControl(m_shooterSubsystem.kickerMotorPercentOutput);
+		
+		m_shooterSubsystem.leaderMotorPercentOutput.withOutput(percent);
+		m_shooterSubsystem.leaderMotor.setControl(m_shooterSubsystem.leaderMotorPercentOutput);
+	}
+
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return isKilled;
+	}
+}
